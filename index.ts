1c64c6cdeafbe6a543d2bf81dc5f807102a823fb
Gustavo Velazquez Moctezuma <tavoo_velazquez@hotmail.com>
2019-06-11 23:50:30 -0500

    Script is ready

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("Hello")
+import * as base64json from 'base64json'
+
+export class VueEasyJwt {
+    private base64: any
+    
+    constructor(){
+        this.base64 = base64json
+    }
+
+    public decodeToken(token: string){
+        // if the token has more or less than 3 parts
+        // then is not a valid token
+        if(token.split('.').length != 3){
+            throw new Error("This is not a valid token")
+        }
+        else {
+            // signature ( part 2 ) has the data stored and
+            // data about the expiration time
+            const signature = token.split('.')[1]
+            // decode and parse to json
+            return this.base64.parse(signature)
+        }
+    }
+
+    public isExpired(token: string): Boolean {
+        // decode the token
+        let decodedToken: any = this.decodeToken(token)
+        let expirationDate: Date = new Date(0)
+        // sets the expiration seconds 
+        expirationDate.setUTCSeconds(decodedToken.exp)
+        
+        // compare the expiration time and the current time
+        return expirationDate.valueOf() < new Date().valueOf()
+    }
+}
